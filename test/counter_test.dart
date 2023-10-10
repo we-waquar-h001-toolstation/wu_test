68633e3e91d5edebc63557e67a25b29d7a7e4500
Waquar Hassan <waquar.hassan@toolstation.com>
2023-10-10 16:53:07 +0530

    Unit testing

---
+++ test/counter_test.dart
@@ -0,0 +1,25 @@
+import 'package:test/test.dart';
+import 'package:wu_testing/counter.dart';
+
+void main() {
+  group('Counter', () {
+    test('value should start at 0', () {
+      expect(Counter().value, 0);
+    });
+
+    test('value should be incremented', () {
+      final counter = Counter();
+
+      counter.increment();
+      expect(counter.value, 2);
+    });
+
+    test('value should be decremented', () {
+      final counter = Counter();
+
+      counter.decrement();
+
+      expect(counter.value, -1);
+    });
+  });
+}
